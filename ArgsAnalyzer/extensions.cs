f652ba7d93ad176f20a19b67e34fbc143fe28f6a
sugaryo <ryo.sugawara.1224@gmail.com>
2020-04-05 21:37:34 +0900

    コードフォーマット（空白文字変換）

--- ArgsAnalyzer/extensions.cs
+++ ArgsAnalyzer/extensions.cs
@@ -6,37 +6,37 @@ using System.Threading.Tasks;
 
 namespace ArgsAnalyzer
 {
-	/// <summary>
-	/// 拡張メソッド
-	/// </summary>
-	public static class extensions
-	{
-		public static bool startsWith( this string s, params string[] with )
-		{
-			foreach ( string w in with )
-			{
-				if ( s.StartsWith( w ) ) return true;
-			}
+    /// <summary>
+    /// 拡張メソッド
+    /// </summary>
+    public static class extensions
+    {
+        public static bool startsWith(this string s, params string[] with)
+        {
+            foreach ( string w in with )
+            {
+                if ( s.StartsWith( w ) ) return true;
+            }
 
-			return false;
-		}
-		public static string[] split( this string s, 
-				params string[] by )
-		{
-			return s.Split( by, StringSplitOptions.None );
-		}
-		public static string[] split( this string s, 
-				StringSplitOptions option, 
-				params string[] by )
-		{
-			return s.Split( by, option );
-		}
+            return false;
+        }
+        public static string[] split(this string s,
+                params string[] by)
+        {
+            return s.Split( by, StringSplitOptions.None );
+        }
+        public static string[] split(this string s,
+                StringSplitOptions option,
+                params string[] by)
+        {
+            return s.Split( by, option );
+        }
 
-		public static Arguments parse( this string[] args )
-		{
-			var arguments = new Arguments();
-			arguments.Load( args );
-			return arguments;
-		}
-	}
+        public static Arguments parse(this string[] args)
+        {
+            var arguments = new Arguments();
+            arguments.Load( args );
+            return arguments;
+        }
+    }
 }
