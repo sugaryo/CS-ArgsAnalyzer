c26d118e3950fc97211801fca081f293de1f33f7
sugaryo <ryo.sugawara.1224@gmail.com>
2020-04-05 19:01:15 +0900

    動作確認ドライバの出力をマークダウン風に変更。

--- ArgsAnalyzerDriver/Program.cs
+++ ArgsAnalyzerDriver/Program.cs
@@ -15,56 +15,62 @@ namespace ArgsAnalyzerDriver
 	{
 		static void Main( string[] args )
 		{
-			Console.WriteLine( "psvm args:" );
+			Console.WriteLine( "# psvm args:" );
 			foreach ( var a in args )
 			{
-				Console.WriteLine( " -" + a );
+				Console.WriteLine( "- " + a );
 			}
-			
-			// Debug args:
-			//   hoge
-			//   moge
-			//   piyo
-			//   -a
-			//   -b
-			//   /c
-			//   /d
-			//   -x=111
-			//   /y=222,333,444
-			// コピペ用：hoge moge piyo -a -b /c /d -x=111 /y=222,333,444
-			Arguments arguments = args.parse();
 
-			Console.WriteLine( "parameters:" );
+
+            Console.WriteLine("# ArgsAnalyzer.Arguments:");
+            // ■ Debug args:
+            //   hoge
+            //   moge
+            //   piyo
+            //   -a
+            //   -b
+            //   /c
+            //   /d
+            //   -x=111
+            //   /y=222,333,444
+            // ■ コピペ用：
+            // hoge moge piyo -a -b /c /d -x=111 /y=222,333,444
+            Arguments arguments = args.parse();
+
+            // 引数の列挙
+			Console.WriteLine( "- parameters:" );
 			var parameters = arguments.AsParameters();
 			foreach ( string p in parameters )
 			{
-				Console.WriteLine( " -" + p );
+				Console.WriteLine( "    - " + p );
 			}
 
-			
-			Console.WriteLine( "options(value):" );
+            // オプション（値型）の列挙
+			Console.WriteLine( "- options(value):" );
 			var v_options = arguments.AsValueOptions();
 			foreach ( ValueOption vo in v_options )
 			{
-				Console.WriteLine( " -" + vo.value );
+				Console.WriteLine( "    - " + vo.value );
 			}
 			
-			Console.WriteLine( "options(property):" );
+            // オプション（属性型）の列挙
+			Console.WriteLine( "- options(property):" );
 			var p_options = arguments.AsPropertyOptions();
 			foreach ( PropertyOption po in p_options )
 			{
-				Console.WriteLine( " -" + po.key + " = " + po.value );
+				Console.WriteLine( "    - " + po.key + " = " + po.value );
 			}
 
 			// Have
-			Console.WriteLine( "have option:" );
-			Console.WriteLine( $" - args[a] : {arguments.Have( "a" )}" );
+			Console.WriteLine( "- have option:" );
+			Console.WriteLine( $"    - args[a] : {arguments.Have( "a" )}" );
 
 			// Prop
-			Console.WriteLine( "prop option:" );
+			Console.WriteLine( "- prop option:" );
 			string x;
 			arguments.Prop( out x, "x" );
-			Console.WriteLine( $" - args[x] : {x ?? "null"}" );
+			Console.WriteLine( $"   -  args[x] : {x ?? "null"}" );
+
 
 
 			Console.WriteLine();
