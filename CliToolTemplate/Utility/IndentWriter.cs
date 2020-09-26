531b15ad63789c928301d1bb2e8611a7cfe61c10
ryota.sugawara <ryo.sugawara.1224@gmail.com>
2020-09-27 07:34:01 +0900

    IndentWriterユーティリティを作ってコードを整理。

---
+++ CliToolTemplate/Utility/IndentWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static CliToolTemplate.Utility.ConsoleAppUtil;
+
+namespace CliToolTemplate.Utility
+{
+    public class IndentWriter
+    {
+        public int Level{ get; set; } = 0;
+
+        public bool Write(string line)
+        {
+            return IndentWrite( this.Level, line );
+        }
+        public bool Write(string[] lines)
+        {
+            return IndentWrite( this.Level, lines );
+        }
+
+        public bool Write(string subtitle, string[] lines)
+        {
+            if ( IndentWrite( this.Level, subtitle ) )
+            {
+                IndentWrite( this.Level + 1, lines );
+                return true;
+            }
+            else
+            {
+                return IndentWrite( this.Level, lines );
+            }
+        }
+    }
+}
