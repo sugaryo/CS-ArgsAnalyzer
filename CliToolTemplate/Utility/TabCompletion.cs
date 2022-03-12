e72b196ae2f30e0bbdfd45cb8d12fb07a5f732e4
sugaryo <ryo.sugawara.1224@gmail.com>
2022-03-12 16:42:23 +0900

    コメント付与と、軽いリファクタリング。

--- CliToolTemplate/Utility/TabCompletion.cs
+++ CliToolTemplate/Utility/TabCompletion.cs
@@ -10,11 +10,16 @@ namespace CliToolTemplate.Utility
     {
         /// <summary>入力補完する候補データ</summary>
         private readonly IEnumerable<string> data;
+
+        #region ctor
         public TabCompletion(IEnumerable<string> data)
         {
             this.data = data;
         }
+        #endregion
 
+
+        #region ReadLine()
         /// <summary>
         /// Console 入力中に Tab キーで入力補完する ReadLine 実装。
         /// </summary>
@@ -38,6 +43,16 @@ namespace CliToolTemplate.Utility
             }
         }
 
+        // Console.ReadKey char 単位の読み込みループ
+        // - [ESC]   入力中断  
+        //           ※ 再帰処理終了
+        // - [ENTER] 入力確定  
+        //           ※ 再帰処理終了
+        // - [BS]    一文字削除  
+        //           ※ カレット移動に対応出来てないので、末尾一文字削除しか出来ない。
+        // - [DEL]   全消し
+        // - [TAB]   前方一致補完：TabComplete
+        // - else    Char.IsControl なら無視し、それ以外は通常入力として buff に詰める。
         private string ReadLineCore(StringBuilder buff)
         {
             // ReadKey を再帰処理して入力を読み取り、補完処理しながら buff に詰めていく。
@@ -91,15 +106,22 @@ namespace CliToolTemplate.Utility
             return ReadLineCore( buff );
         }
 
+        // [TAB] 入力時の補完処理：
+        // - this.data の中から string.StartsWith で前方一致判定するものを抜き出す。
+        // - 抜き出した件数に応じて分岐
+        //   - １件ヒットした場合は確定なので、ヒットしたものを入力する。
+        //   - 複数ヒットした場合は、それらの共通部分まで入力補完する。
+        //   - ヒットしなかった場合は何もしない。
         private void TabComplete(StringBuilder buff)
         {
-            // データから前方一致するものを抜き出し。
-            string prefix = buff.ToString();
+            // データから前方一致するものを抜き出し、ヒットした件数に応じてコンソール処理する。
+            string input = buff.ToString();
             var matches = this.data
-                .Where( x => x.StartsWith( prefix ) )
+                .Where( x => x.StartsWith( input ) )
                 .ToArray();
 
 
+            // ■１件（確定）
             if ( 1 == matches.Length )
             {
                 // 前方一致が１件しか無ければこれで確定。
@@ -107,6 +129,7 @@ namespace CliToolTemplate.Utility
                 buff.reset( match );
                 OverWrite( buff.ToString() );
             }
+            // ■複数（絞り込まれたものの中から共通部分まで入力を補完する）
             else if ( 1 < matches.Length )
             {
                 Console.ForegroundColor = ConsoleColor.Blue;
@@ -119,16 +142,24 @@ namespace CliToolTemplate.Utility
                 Console.ResetColor();
 
                 // 前方一致が複数ある場合は共通部分まで入力補完。
-                string match = FowardMatcher.Like( matches );
-                buff.reset( match );
+                string prefix = FowardMatcher.Like( matches );
+                buff.reset( prefix );
                 OverWrite( buff.ToString() );
             }
+            // ■ヒットするものなし（なにもしない）
             else
             {
                 // マッチするものがなかったらこのまま。
             }
         }
+        #endregion
+        
 
+        #region static OverWrite()
+        // 指定した文字列 str で完全に上書きする。
+        // Consoler 自体にそういう機能はなかったので、
+        // 一旦行全体にスペース書き込みして見た目消して、その上から str を書き込む事にした。
+        // ※ 全角半角問題があるので str.Length は使えないので Console.BufferWidth を採用。
         private static void OverWrite(string str)
         {
             // 一旦スペースを書き込んで見た目上消す。
@@ -139,8 +170,9 @@ namespace CliToolTemplate.Utility
             Console.CursorLeft = 0;
             Console.Write( str );
         }
+        #endregion
 
-        #region private static class FowardMatcher
+        #region static class FowardMatcher
         /// <summary>
         /// 複数の候補データの全てが前方一致している部分を抜き出す。
         /// </summary>
